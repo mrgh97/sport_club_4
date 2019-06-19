008bc5b32844dffbd6e174e7f4ff1cc87916ef72
mrgh97 <719475327@qq.com>
2019-06-19 21:31:31 +0800

    change

--- src/main/java/com/example/jpademo/service/WorkerService.java
+++ src/main/java/com/example/jpademo/service/WorkerService.java
@@ -1,18 +1,13 @@
 package com.example.jpademo.service;
 
-import java.util.List;
-
-import com.example.jpademo.domain.Member;
 import com.example.jpademo.domain.Worker;
 import com.example.jpademo.repository.WorkerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Service
@@ -24,44 +19,29 @@ public class WorkerService {
 		this.workerRepository=w;
 	}
 
-	@Transactional
-	public Worker getById(Integer id) {
-		return workerRepository.getOne(id);
+	public Flux<Worker> getAllWorkers(){
+		return workerRepository.findAll();
 	}
 
 	@Transactional
-	public void deleteWorker(Integer WorkerId) {
-		workerRepository.deleteById(WorkerId);
+	public Mono<Worker> getById(Integer id) {
+		return workerRepository.findById(id);
 	}
 
 	@Transactional
-	public void addWorker(Worker Worker) {
-		workerRepository.save(Worker);
+	public Mono<Void> deleteWorker(Integer WorkerId) {
+		return workerRepository.deleteById(WorkerId);
 	}
 
 	@Transactional
-	public boolean updateWorker(Worker Worker) {
-		return workerRepository.save(Worker) != null;
+	public Mono<Worker> addWorker(Worker Worker) {
+		return workerRepository.save(Worker);
 	}
 
 	@Transactional
-	public Page<Worker> getAllWorkers(Integer pageNum){
-		Sort sort = new Sort(Sort.Direction.ASC,"id");
-		Pageable pageable=new PageRequest(pageNum,2,sort);
-
-		return this.workerRepository.findAll(pageable);
+	public Mono<Worker> updateWorker(Worker Worker) {
+		return workerRepository.save(Worker);
 	}
 
-	@Transactional
-	public Page<Worker> getAll(Pageable pageable) {
-		return this.workerRepository.findAll(pageable);
-	}
-
-	public void addMember(Member member,Integer workerId){
-		Worker worker = workerRepository.findWorkerById(workerId);
-		List<Member> memberList=worker.getMembers();
-		memberList.add(member);
-		worker.setMembers(memberList);
-	}
 
 }
