f633957b49a64d3f5b34ee13e3251299d37832c2
mrgh97 <719475327@qq.com>
2019-06-19 21:33:37 +0800

    login

--- src/main/java/com/example/jpademo/controller/LoginController.java
+++ src/main/java/com/example/jpademo/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.example.jpademo.controller;
 
-import com.example.jpademo.controller.util.HeaderUtil;
 import com.example.jpademo.service.MemberService;
 import com.example.jpademo.domain.Member;
 import io.swagger.annotations.Api;
@@ -64,71 +63,5 @@ public class LoginController {
             e.printStackTrace();
         }
     }
-    /**
-     * POST  /videos : Create a new video.
-     *
-     * @param username
-     * @param password
-     * @return the ResponseEntity with status 201 (Created) and with body the new videoDTO, or with status 400 (Bad Request) if the video has already an ID
-     */
-    @ApiOperation("验证登录信息")
-    @ApiImplicitParams({@ApiImplicitParam(dataType = "String",name = "username",value = "用户名",required = true),
-                                            @ApiImplicitParam(dataType = "String",name = "password",value = "密码",required = true)})
-    @PostMapping(value = "/login")
-    public ResponseEntity<Member> verifyLogin(@RequestParam("username") String username,
-                                            @RequestParam("password") String password,
-                                            HttpServletRequest request) {
-        log.debug("Rest to verify login");
-        HttpSession session = request.getSession();
-        if (session.getAttribute("user") != null) {
-            return ResponseEntity.badRequest()
-                    .headers(HeaderUtil.createAlert("Already login.", username))
-                    .build();
-        }
-        if (mService.checkLogin(username, password)) {
-            Member member = mService.findMember(username);
-            session.setAttribute("user", member);
-            return ResponseEntity.ok().body(member);
-        }
-        return ResponseEntity.badRequest().headers(HeaderUtil.createAlert("Username or password error", username)).build();
-    }
 
-    @ApiOperation("注册")
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<Member> register(@Valid @ModelAttribute(value = "member") Member member,
-                           BindingResult bindingResult,
-                           @RequestParam("rePassword") String rePassword,
-                           HttpServletRequest request) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createAlert("Input contents has error", member.getUserName())).build();
-        } else if (!member.getPassword().equals(rePassword)) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createAlert("Username or password error", member.getUserName())).build();
-        } else {
-            member.setSign("normal");
-            mService.addMember(member);
-            HttpSession session = request.getSession();
-            session.setAttribute("user", member);
-            return ResponseEntity.ok()
-                    .headers(HeaderUtil.createEntityCreationAlert(entityName, member.getUserName().toString()))
-                    .body(member);
-        }
-    }
-
-    @ApiOperation("登出")
-    @GetMapping("/logOut")
-    public void logOut(HttpServletRequest request) {
-        log.debug("Rest request to logout");
-        HttpSession session = request.getSession();
-        session.removeAttribute("user");
-    }
-//    @RequestMapping(value = "/register", method = RequestMethod.GET)
-//    public String register(Model model) {
-//        model.addAttribute("member", new Member());
-//        return "login/register";
-//    }
-
-//    @RequestMapping(value = "/userCenter", method = RequestMethod.GET)
-//    public String profile() {
-//        return "login/profile";
-//    }
 }
